9e4e4c5be7e60033c84ecccbd45e2f56f7bd192b
wangmta <wangmta@gmail.com>
2020-05-20 15:01:28 -0400

    feat: add indexOf array

--- leetcode/indexOf.js
+++ leetcode/indexOf.js
@@ -8,7 +8,27 @@ var indexOf = function (haystack, needle) {
     return -1;
 }
 
-console.log(indexOf('hello', 'll'));
-console.log(indexOf('flower', 'we'));
-console.log(indexOf('nananana', 'ab'));
-console.log(indexOf('nananana', ''));
+// console.log(indexOf('hello', 'll'));
+// console.log(indexOf('flower', 'we'));
+// console.log(indexOf('nananana', 'ab'));
+// console.log(indexOf('nananana', ''));
+
+// Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You may assume no duplicates in the array.
+var indexOfArray = function (nums, target) {
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (target <= nums[i]) {
+            return i;
+        }
+        if (target > nums[i]) {
+            index++;
+        }
+    }
+    return index;
+}
+
+console.log(indexOfArray([1, 3, 5, 6], 5));
+console.log(indexOfArray([1, 3, 5, 6], 2));
+console.log(indexOfArray([1, 3, 5, 6], 7));
+console.log(indexOfArray([1, 3, 5, 6], 0));
